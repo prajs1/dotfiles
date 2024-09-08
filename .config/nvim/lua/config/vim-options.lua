3e612d1239bdbf754c3d17158f2d671eb5581f75
Robert <71959725+prajs1@users.noreply.github.com>
2024-09-08 17:53:44 +0200

    Create vim-options.lua

---
+++ .config/nvim/lua/config/vim-options.lua
@@ -0,0 +1,35 @@
+-- Left column / wrap / scrolloff
+vim.opt.number = true -- display line numbers
+vim.opt.relativenumber = true -- desplay relative line numbers 
+vim.opt.signcolumn  = "yes" -- always show sign column
+vim.opt.wrap = true -- ebale lines wrapping if exceeds window width
+vim.opt.scrolloff = 10 -- Number of columns to keep above/below cursor
+
+-- Tab spacing/behavior
+vim.opt.expandtab = true -- convert tabs to spaces
+vim.opt.tabstop = 2 -- number of spaces inserted for each tab character 
+vim.opt.softtabstop = 2 -- number of spaces inserted for each <Tab> key
+vim.opt.shiftwidth = 2 -- number of spaces inserted for every indentation level
+vim.opt.smartindent = true -- enable smart indentation
+vim.opt.breakindent = true -- enable line breaking indentation
+
+-- General behavior
+vim.opt.clipboard = "unnamedplus" -- enable system clipboard access
+vim.opt.fileencoding = "utf-8" -- set file encoding to UTF-8
+vim.opt.mouse = "a" -- enable mouse support
+vim.opt.showmode = false -- hide mode display
+vim.opt.termguicolors = true -- enable term GUI colors
+vim.opt.timeoutlen = 1000 -- set timeout for mapped sequence [in ms]
+vim.opt.undofile = true -- enable persistent undo
+vim.opt.updatetime = 100 -- set faster completion [in ms]
+vim.opt.cursorline = true -- highlight current line
+
+--Window splitting
+vim.opt.splitbelow = true -- force horizontal splits below current Window
+vim.opt.splitright = true -- forse vertical splits on the right of current window
+
+-- Searching behaviors
+vim.opt.hlsearch = true -- highlight all mathces in search
+vim.opt.ignorecase = true -- ignore casesensitive in search
+vim.opt.smartcase = true -- match casesensitive if explicitly stated
+
