b4bb5af2017946bced9a50c5a20999fedd083d0e
Robert <71959725+prajs1@users.noreply.github.com>
2024-09-08 17:52:06 +0200

    Update init.lua
    Added comments and loaded options/keymaps for Vim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,6 @@
+-- Declare the apth where Lazy will clone plugin code
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+-- Check if Lazy is cloned if not then clone it
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
     "git",
@@ -9,7 +11,14 @@ if not vim.loop.fs_stat(lazypath) then
     lazypath,
   })
 end
+-- Add the path to Lazy plugins repositories to the Vim runtime path
 vim.opt.rtp:prepend(lazypath)
 
-require("vim-options")
+-- Load options for Vim from config/vim-options.lua
+require("config.vim-options")
+-- Load keymaps for Vim from config/vim-keymaps.lua
+require("config.vim-keymaps")
+-- Setup Lazy, this always should be last
+-- Tell Lazy that all plugins specs are located in plugins directory
+-- We can also specify lazy options by creating and adding local variable called opts and adding it after comma in setup
 require("lazy").setup("plugins")
