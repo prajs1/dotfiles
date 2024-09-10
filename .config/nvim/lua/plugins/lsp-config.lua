0ba3a96cd27591b023b7bfe54bad816fbfea6232
Robert <71959725+prajs1@users.noreply.github.com>
2024-09-10 17:43:25 +0200

    Update lsp-config.lua
    Added keymaps and eslint LSP

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -25,11 +25,18 @@ return {
       lspconfig.tsserver.setup({
         capabilities = capabilities
       })
+      
+      lspconfig.eslint.setup({
+        capabilities = capabilities,
+      })
 
-      vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-      vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {})
-      vim.keymap.set({ 'n' }, '<leader>ca', vim.lsp.buf.code_action, {})
+      vim.keymap.set("n", "<leader>ch", vim.lsp.buf.hover, { desc = "[C]ode [H]over Documentation" })
+      vim.keymap.set("n", "<leader>cd", vim.lsp.buf.definition, { desc = "[C]ode Goto [D]efinition" })
+      vim.keymap.set("n", "<leader>ci", vim.lsp.buf.implementation, { desc = "[C]ode Goto [I]mplementation" })
+      vim.keymap.set({ "n" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "[C]ode [A]ction" })
+      vim.keymap.set("n", "<leader>cr", require("telescope.builtin").lsp_references, { desc = "[C]ode Goto [R]eferences" })
+      vim.keymap.set("n", "<leader>cR", vim.lsp.buf.rename, { desc = "[C]ode [R]ename" })
+      vim.keymap.set("n", "<leader>cD", vim.lsp.buf.declaration, { desc = "[C]ode Goto [D]eclaration" })
     end
   }
 }
