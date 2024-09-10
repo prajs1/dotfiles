5338525495d2279f2e571bdbea8761643720f602
Robert <71959725+prajs1@users.noreply.github.com>
2024-09-10 17:40:47 +0200

    Update treesitter.lua

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua","javascript","typescript","java","python","css","scss","html","markdown","markdown_inline"},
+      ensure_installed = {"lua","javascript","typescript","java","python","css","scss","html","markdown","markdown_inline", "json"},
       auto_install = true,
       highlight = { enable = true },
       indent = { enable = true },
