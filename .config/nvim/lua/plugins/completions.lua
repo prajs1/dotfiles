8f0ddf9063496304388e0d3453d8226b03bb4422
Robert <71959725+prajs1@users.noreply.github.com>
2024-09-11 20:18:23 +0200

    Update completions.lua

--- .config/nvim/lua/plugins/completions.lua
+++ .config/nvim/lua/plugins/completions.lua
@@ -11,11 +11,26 @@ return {
 	},
 	{
 		"hrsh7th/nvim-cmp",
+    dependencies = {
+      -- buffer based completion options
+      "hrsh7th/cmp-buffer",
+      -- path based completion options
+      "hrsh7th/cmp-path",
+    },
 		config = function()
 			local cmp = require("cmp")
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
+        -- How should completion options be displayed
+        completion = {
+          -- menu: display options in a menu
+          -- menuone: automatically select the first option of the menu
+          -- preview: automatically display the completion candidate while navigating the menu
+          -- noselect: prevent neovim from automatically selecting a completion option while navigating the menu
+          competeopt = "menu,menuone,preview,noselect"
+        },
+        -- Setup snippet support based on the active lsp and the current text of the file
 				snippet = {
 					expand = function(args)
 						require("luasnip").lsp_expand(args.body)
@@ -26,6 +41,8 @@ return {
 					documentation = cmp.config.window.bordered(),
 				},
 				mapping = cmp.mapping.preset.insert({
+          -- ["<C-k>"] = cmp.mapping.select_prev_item(),
+          -- ["<C-j>"] = cmp.mapping.select_next_item(),
 					["<C-b>"] = cmp.mapping.scroll_docs(-4),
 					["<C-f>"] = cmp.mapping.scroll_docs(4),
 					["<C-Space>"] = cmp.mapping.complete(),
@@ -35,6 +52,8 @@ return {
 				sources = cmp.config.sources({
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
+          { name = "buffer" },
+          { name = "path" }
 				}, {
 					{ name = "buffer" },
 				}),
