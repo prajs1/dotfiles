33d6c4fc19118da68d7253a533ffef5152eb4512
Robert Prajs <71959725+prajs1@users.noreply.github.com>
2024-09-27 09:40:41 +0200

    Changed configuration for catppuccin theme

--- .config/nvim/lua/plugins/catppuccin.lua
+++ .config/nvim/lua/plugins/catppuccin.lua
@@ -3,6 +3,18 @@ return {
   name = "catppucin",
   priority = 1000,
   config = function()
+    require("catppuccin").setup({
+      flavour = "frappe",
+      transparent_background = true,
+      no_italic = false,
+      no_bold = false,
+      no_underline = false,
+      styles = {
+        comments = { "italic" },
+        conditionala = { "italic" },
+      },
+    })
+
     vim.cmd.colorscheme "catppuccin"
   end
 }
