0648bf0d325808b47750da506665d4ff1d0c4c66
Robert Prajs <prajs.robert@gmail.com>
2025-12-09 14:13:22 +0100

    MODIFY: treesitter context changes

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,22 +1,41 @@
 return {
-    {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function()
-    local config = require("nvim-treesitter.configs")
-    config.setup({
-      ensure_installed = {"lua","javascript","typescript","java","python","css","scss","html","php","markdown","markdown_inline", "json", "bash", "diff", "xml"},
-      auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
-    })
-  end
-},
-    {
-        "nvim-treesitter/nvim-treesitter-context",
-        config = function ()
-            local config = require("treesitter-context")
-            config.setup()
-        end
-    }
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			local config = require("nvim-treesitter.configs")
+			config.setup({
+				ensure_installed = {
+					"lua",
+					"javascript",
+					"typescript",
+					"java",
+					"python",
+					"css",
+					"scss",
+					"html",
+					"php",
+					"markdown",
+					"markdown_inline",
+					"json",
+					"bash",
+					"diff",
+					"xml",
+				},
+				auto_install = true,
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-context",
+		config = function()
+			local config = require("treesitter-context")
+			config.setup({
+				-- max_lines = 5,
+                multiline_threshold = 5,
+			})
+		end,
+	},
 }
