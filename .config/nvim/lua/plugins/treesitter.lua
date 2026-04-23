0f1ad0d1bda12fcf417e9954868c61ba0a4dd861
Robert Prajs <prajs.robert@gmail.com>
2026-04-23 13:10:24 +0200

    MODIFY: Changed treesitter branch to master

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,8 @@
 return {
 	{
 		"nvim-treesitter/nvim-treesitter",
+        lazy = false,
+        branch = "master",
 		build = ":TSUpdate",
 		config = function()
 			local config = require("nvim-treesitter.configs")
